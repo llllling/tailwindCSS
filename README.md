5d0d555ed7ae6633962e48c4e45c3579a24c82ff
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-10-26 17:45:23 +0900

    feat : support 생성

--- README.md
+++ README.md
@@ -98,3 +98,12 @@ npx tailwindcss init -p
      ```
 
 * **즉, postCSS는 툴인데, 이런 툴 안에서 위 4가지에 적용된 플러그인들뿐만 아니라 많은 플러그인들을 사용하여 css를 변환을 시켜주는 것이다.**
+
+## 진행하면서 알게된 것들.
+
+- object-fit : img나 video 태그의 크기를 조정하는 방법으로 사용됨.(참고 : https://ossam5.tistory.com/124)
+
+* mix-blend-mode : 요소의 내용이 부모 배경과 혼합되는 방법 지정(참고 : http://www.devdic.com/css/refer/properties/property:1193/mix-blend-mode)
+  - overlay : 겹쳐진 두 요소중 뒤쪽의 색이 더 어두운 경우에 multiply, 더 밝은 경우에는 screen으로 혼합하여 색상을 표시한다.
+  - multiply : 겹쳐 있는 두 요소의 색상을 곱한 값으로 표시한다. 결과적으로 어둡게 색이 보여진다.
+  - screen : 겹쳐진 두 요소의 색상을 각각 반전한 후 서로 곱해 나온 색상을 다시 반전하여 색상을 표시한다.multiply와 반대 결과로 보이며 더 밝은 혼합으로 처리한다.
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,5 +1,74 @@
 import React from "react";
+import SUPPORT from "../assets/support.jpeg";
+import { FaArrowRight } from "react-icons/fa";
 
 export const Support = () => {
-  return <div>Support</div>;
+  return (
+    <div name="support" className="w-full mt-24">
+      <div className="w-full h-[700px] bg-slate-900/90 absolute">
+        <img
+          className="object-cover w-full h-full mix-blend-overlay"
+          src={SUPPORT}
+          alt="support_background"
+        />
+      </div>
+      <div className="max-w-[1240px] mx-auto text-white relative">
+        <div className="px-4 py-12">
+          <h2 className="pt-8 text-3xl text-center uppercase text-slate-300">
+            support
+          </h2>
+          <h3 className="py-6 font-bold text-5xl text-center">
+            We support you
+          </h3>
+
+          <p className="py-4 text-3xl text-slate-300">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.{" "}
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 px-4 py-12 text-black lg:grid-cols-3 gap-x-8 gap-y-16 sm:pt-20">
+          <SupportItem
+            title="Support"
+            description=" Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+          />
+          <SupportItem
+            title="Inquiries"
+            description=" Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+          />
+          <SupportItem
+            title="Sales"
+            description=" Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+function SupportItem({ title, description }) {
+  return (
+    <div className="bg-white shadow-2xl rounded-xl">
+      <div className="p-8">
+        <h3 className="my-6 font-bold text-2xl">{title}</h3>
+        <p className="text-xl text-gray-600">{description}</p>
+      </div>
+      <div className="py-4 pl-8 bg-slate-100">
+        <p className="flex items-center text-blue-600">
+          More Info... <FaArrowRight className="w-5 ml-2" />
+        </p>
+      </div>
+    </div>
+  );
+}
