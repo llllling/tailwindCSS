58cea99baa536c0c246ace335c29a26eea5470f4
MISEONGKIM <rlaaltjd46@naver.com>
2023-10-29 22:17:17 +0900

    feat : footer 디자인 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaTwitch,
+  FaGithub,
+} from "react-icons/fa";
 const FOOTER_LIST = [
   {
     title: "solutions",
@@ -19,12 +26,35 @@ const FOOTER_LIST = [
 ];
 export const Footer = () => {
   return (
-    <section className="w-full my-3 text-white bg-slate-700">
-      <div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-4">
-          {FOOTER_LIST.map((data, i) => (
-            <FooterItem key={i} data={data} />
-          ))}
+    <section className="w-full px-2 py-7 text-gray-300 bg-slate-700">
+      <div className="max-w-[1240px] py-8 mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 ">
+        {FOOTER_LIST.map((data, i) => (
+          <FooterItem key={i} data={data} />
+        ))}
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className=" font-bold uppercase">subscribe to our newsletter</p>
+          <p className="py-4">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row ">
+            <input
+              className="w-full p-2 mb-4 mr-4 rounded-sm"
+              type="email"
+              placeholder="Enter email.."
+            />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between sm:flex-row max-w-[1240px] mx-auto px-2 py-4 text-center text-gray-500">
+        <p className="py-4">2022 Chatty, LLC. All rights reserved</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          <FaFacebook />
+          <FaInstagram />
+          <FaTwitter />
+          <FaTwitch />
+          <FaGithub />
         </div>
       </div>
     </section>
@@ -33,12 +63,12 @@ export const Footer = () => {
 
 function FooterItem({ data }) {
   return (
-    <div>
-      <h3>{data.title}</h3>
+    <div className="text-center">
+      <h6 className="pt-2 font-bold uppercase">{data.title}</h6>
       {data.items.map((item, i) => (
-        <div key={i}>
-          <a>{item}</a>
-        </div>
+        <ul key={i}>
+          <li className="py-1">{item}</li>
+        </ul>
       ))}
     </div>
   );
